3b5ff2c8a306f9272532c4afe452674b08565bb1
Seun1601 <seunfadairo76@gmail.com>
2023-05-19 20:58:23 +0100

    html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 
+import Nav from "../components/nav" ;
+
 
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-     <h4>ELD Unilag</h4>
+      <div>
+        <Nav />
+        <h1>Welcome to the Home page</h1>
+      </div>
     </main>
   )
 }
