132323a9d78864056a2f3d751df0af094d90b37a
David Bajomo <bajomodavid18@gmail.com>
2023-07-11 13:00:54 +0100

    superheroes section for desktop

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
-import Nav from '@/components/Header/nav'
-import Footer from '@/components/Footer/footer'
+import Nav from '@/components/header/nav'
+import Footer from '@/components/footer/footer'
 
 
 export default function App({ Component, pageProps }) {
