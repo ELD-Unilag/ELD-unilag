618cbb2743cf475629017e141ef4137d85e4c453
Nicholas <102188095+nick335@users.noreply.github.com>
2023-08-12 13:43:02 +0100

    wcw route added!

---
+++ src/pages/admin/editwcw.js
@@ -0,0 +1,14 @@
+import Layout from '@/components/Admin/Wcw/Layout'
+import GoBackBtn from '@/components/Utility/AdminUtilities/GoBackBtn/GoBackBtn'
+import React from 'react'
+
+const editwcw = () => {
+  return (
+    <main className='adminPagesLayout'>
+     <GoBackBtn />
+     <Layout />
+    </main>
+  )
+}
+
+export default editwcw
