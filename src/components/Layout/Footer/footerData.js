62d2b9f8e6bf280e67ffa471c012ef9d0e52f3ff
[nick335] <[chigozieerigo@gmail.com]>
2023-09-12 13:03:38 +0100

    images renamed!

--- src/components/Layout/Footer/footerData.js
+++ src/components/Layout/Footer/footerData.js
@@ -1,7 +1,7 @@
 import Instagram from '../../../../public/images/footerImages/instagramIcon.png'
 import Facebook from '../../../../public//images/footerImages/facebookIcon.png'
 import Twitter from '../../../../public/images/footerImages/twitterIcon.png'
-import Linkedin from '../../../../public/images/footerImages/LinkedinIcon.png'
+import Linkedin from '../../../../public/images/footerImages/linkedinIcon.png'
 
 
 export const socialLinks =[
